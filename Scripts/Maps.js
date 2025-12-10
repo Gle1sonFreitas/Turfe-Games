c6a84207d18d1f1245c02399b09c1c8158c40b56
Gustavo982-web <gustavo.ssantos@sptech.school>
2025-12-09 21:49:34 -0300

    tela de seleção de mapas

---
+++ Scripts/Maps.js
@@ -0,0 +1,66 @@
+ var listarmapas = [
+    {
+        nome: 'Mundo antigo',
+        imagem: '<img class="options" src="../Assets/img/Mapa1.png" alt="">',
+        status: 1
+    },
+    {
+        nome: 'Nova Vida',
+        imagem: '<img class="options" src="../Assets/img/Mapa2.png" alt="">',
+        status: 1
+    },
+    {
+        nome: 'Casa dos cavalhos',
+        imagem: '<img class="options" src="../Assets/img/Mapa3.png" alt="">',
+        status: 1
+    }
+]
+
+var player01 = document.getElementById('imgPlayer1')
+var player02 = document.getElementById('imgPlayer2')
+var player03 = document.getElementById('imgPlayer3')
+
+var NomePlayer1 = document.getElementById("NomePlayer1")
+var NomePlayer2 = document.getElementById("NomePlayer2")
+var NomePlayer3 = document.getElementById("NomePlayer3")
+
+var btnPlayer1 = document.getElementById('btnPlayer1')
+var btnPlayer2 = document.getElementById('btnPlayer2')
+var btnPlayer3 = document.getElementById('btnPlayer3')
+
+var readyActive1 = false
+var readyActive2 = false
+var readyActive3 = false
+
+
+function CarregarMapas() {
+    player01.innerHTML = listarmapas[0].imagem;
+    NomePlayer1.innerHTML = listarmapas[0].nome;
+
+    player02.innerHTML = listarmapas[1].imagem;
+    NomePlayer2.innerHTML = listarmapas[1].nome;
+
+    player03.innerHTML = listarmapas[2].imagem;
+    NomePlayer3.innerHTML = listarmapas[2].nome;
+
+}
+
+var playersActive = 0
+
+function Come(player) {
+    if (player == 1) {
+        sessionStorage.MAPA_SELECTED = 0;
+    } else if (player == 2) {
+        sessionStorage.MAPA_SELECTED = 1;
+    } else if (player == 3) {
+        sessionStorage.MAPA_SELECTED = 2;
+    } else {
+        sessionStorage.MAPA_SELECTED = 3;
+    }
+
+        alert("Mapa selecionado: " + listarmapas[sessionStorage.MAPA_SELECTED].nome);
+
+}
+CarregarMapas();
+
+
